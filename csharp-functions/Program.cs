6fea853f34917faffc23e4d9ffb75bd402c9bb41
Matteo Emanuele Russo <matteoh8@hotmail.it>
2023-03-17 15:11:33 +0100

    Esercizi finiti

--- csharp-functions/Program.cs
+++ csharp-functions/Program.cs
@@ -1,6 +1,9 @@
-﻿// Prima funzione
-Console.WriteLine("PRIMA FUNZIONE ");
-int[] numeri = { 1, 2, 3, 4, 5 };
+﻿using System;
+
+int[] numeri = { 2, 6, 7, 5, 3, 9 };
+
+// Stampare l’array di numeri fornito a video
+Console.WriteLine("PRIMO ESERCIZIO ");
 StampaArray(numeri);
 void StampaArray(int[] array)
 {
@@ -17,49 +20,66 @@ void StampaArray(int[] array)
 }
 Console.WriteLine("");
 
-// Seconda funzione
-Console.WriteLine("SECONDA FUNZIONE ");
-Console.WriteLine("Inserisci un numero");
-int numero = Convert.ToInt32(Console.ReadLine());
-int quadrato = Quadrato(numero);
-Console.WriteLine(quadrato);
-int Quadrato(int numero)
-{
-    return numero * numero;
-}
-Console.WriteLine("");
+// Stampare l’array di numeri fornito a video, dove ogni numero è stato prima elevato al quadrato 
+Console.WriteLine("SECONDO ESERCIZIO ");
 
-// Terza funzione
-Console.WriteLine("TERZA FUNZIONE ");
-int[] numbers = { 1, 2, 3, 4, 5 };
-int[] numeriAlQuadrato = ArrayAlQuadrato(numbers);
-Console.WriteLine("");
-StampaArray(numeriAlQuadrato);
-int[] ArrayAlQuadrato(int[] array)
+int[] numeriQuadrati = Quadrati(numeri);
+int[] Quadrati(int[] numeri)
 {
-    int[] nuovoArray = new int[array.Length];
-    for (int i = 0; i < array.Length; i++)
+    int[] numeriQuadrati = new int[numeri.Length];
+
+    for (int i = 0; i < numeri.Length; i++)
     {
-        nuovoArray[i] = array[i] * array[i];
-        Console.Write($"{nuovoArray[i]} ");
+        numeriQuadrati[i] = numeri[i] * numeri[i];
     }
-    return nuovoArray;
+
+    return numeriQuadrati;
+}
+Console.Write("Array con i quadrati: ");
+foreach (int num in numeriQuadrati)
+{
+    Console.Write($"{num} ");
 }
 Console.WriteLine("");
 
+Console.Write("Array originale: ");
+foreach (int num in numeri)
+{
+    Console.Write($"{num} ");
+}
+Console.WriteLine("\n");
 
-// Quarta funzione
-Console.WriteLine("QUARTA FUNZIONE ");
-int[] Sum = { 1, 2, 3, 4, 5 };
-Somma(Sum);
-int Somma(int[] Sum)
+
+
+// Stampare la somma di tutti i numeri
+Console.WriteLine("TERZO ESERCIZIO ");
+Somma(numeri);
+int Somma(int[] numeri)
 {
     int sum = 0;
-    foreach (int num in Sum)
+    foreach (int num in numeri)
     {
         sum += num;
     }
-    Console.WriteLine($"La somma é {sum}.");
+    Console.WriteLine($"La somma di tutti i numeri é {sum}.");
+    return sum;
+}
+Console.WriteLine("");
+
+
+
+// Stampare la somma di tutti i numeri elevati al quadrati
+Console.WriteLine("QUARTO ESERCIZIO ");
+
+SommaQuadrati(numeriQuadrati);
+int SommaQuadrati(int[] numeriQuadrati)
+{
+    int sum = 0;
+    foreach (int num in numeriQuadrati)
+    {
+        sum += num;
+    }
+    Console.WriteLine($"La somma di tutti i numeri al quadrato é {sum}.");
     return sum;
 }
 Console.WriteLine("");
