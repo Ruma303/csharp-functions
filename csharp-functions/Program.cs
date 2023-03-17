ca0fc6e7be391b7216447c734abda137fdc5b92a
Matteo Emanuele Russo <matteoh8@hotmail.it>
2023-03-17 14:22:44 +0100

    prima funzione

--- csharp-functions/Program.cs
+++ csharp-functions/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿//Prima funzione
+
+int[] numeri = { 1, 2, 3, 4, 5 };
+StampaArray(numeri);
+void StampaArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//int Quadrato(int numero);
