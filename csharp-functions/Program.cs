4c9a741cab9422d448777371dd688bffa36e982b
Matteo Emanuele Russo <matteoh8@hotmail.it>
2023-03-17 14:35:26 +0100

    terza funzione

--- csharp-functions/Program.cs
+++ csharp-functions/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Prima funzione
-
+Console.WriteLine("PRIMA FUNZIONE ");
 int[] numeri = { 1, 2, 3, 4, 5 };
 StampaArray(numeri);
 void StampaArray(int[] array)
@@ -15,9 +15,10 @@ void StampaArray(int[] array)
     }
     Console.WriteLine("]");
 }
-
+Console.WriteLine("");
 
 // Seconda funzione
+Console.WriteLine("SECONDA FUNZIONE ");
 Console.WriteLine("Inserisci un numero");
 int numero = Convert.ToInt32(Console.ReadLine());
 int quadrato = Quadrato(numero);
@@ -26,3 +27,22 @@ int Quadrato(int numero)
 {
     return numero * numero;
 }
+Console.WriteLine("");
+
+// Terza funzione
+Console.WriteLine("TERZA FUNZIONE ");
+int[] numbers = { 1, 2, 3, 4, 5 };
+int[] numeriAlQuadrato = ArrayAlQuadrato(numbers);
+Console.WriteLine("");
+StampaArray(numeriAlQuadrato);
+int[] ArrayAlQuadrato(int[] array)
+{
+    int[] nuovoArray = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        nuovoArray[i] = array[i] * array[i];
+        Console.Write($"{nuovoArray[i]} ");
+    }
+    return nuovoArray;
+}
+Console.WriteLine("");
